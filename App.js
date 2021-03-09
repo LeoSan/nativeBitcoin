cd73697b690e718712062efa591e277ea7036b31
LeoSan <cuenca623@gmail.com>
2021-03-09 00:04:50 -0600

    Autor: Leonard, Descripcion: Como incoporar un select, como usar un servicio web con axios.

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 
-import React from 'react';
+import React,  {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Header from './componets/Header';
 import Formulario from './componets/Formulario';
+import axios from 'axios';
 
 
 export default function App() {
+
+    //Generamos nuestros useSate
+    const [moneda,    setMoneda] = useState('');    
+    const [cripto,    setCripto] = useState('');  
+    const [consultarAPI,  setConsultarAPI] = useState(false);  
+
+
+    useEffect(()=>{
+      
+     const cotizarValor = async ()=>{
+
+          if (consultarAPI === true ){
+            console.log('Listo para consultar');
+
+            const url       = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cripto}&tsyms=${moneda}`;
+            const resultado = await axios.get(url);
+
+            console.log(resultado.data.DISPLAY);
+
+          }      
+
+          
+     }
+     cotizarValor();
+
+    }, [consultarAPI])  
+
   return (
     <>
         <Header />
@@ -15,7 +43,14 @@ export default function App() {
         />
 
         <View  style={styles.contenido}>
-            <Formulario/>
+            <Formulario
+                moneda    =   {moneda}
+                cripto    =   {cripto}
+                setMoneda =   {setMoneda}
+                setCripto =   {setCripto}
+                setConsultarAPI =   {setConsultarAPI}
+
+            />
         </View>
     </>
 
--- componets/Formulario.js
+++ componets/Formulario.js
@@ -1,12 +1,117 @@
-import React from 'react'; 
-import {Text, StyleSheet, Platform, View} from 'react-native';
+import React, {useState, useEffect} from 'react'; 
+import {Text, StyleSheet, Platform, View, TouchableHighlight, Alert} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import axios from 'axios';
 
 
-const Formulario = () => {
+const Formulario = ({moneda, cripto, setMoneda, setCripto, setConsultarAPI}) => {
+
+  
+    const [criptos,   setCriptos] = useState([]);    
+
+
+    
+    //Listado de eventos 
+    //Este evento guarda la seleccion en el state 
+    const obtenerMoneda = (moneda)=>{
+        console.log(moneda);
+        setMoneda(moneda);
+    }
+
+
+    //Almacena en el state la opción del usuario. 
+    const obtenerCripto = (cripto)=>{
+        console.log(cripto);
+        setCripto(cripto);
+    }    
+
+    //
+    const cotizarPrecio =()=>{
+
+        console.log("cotizando");
+        if( moneda.trim() ==='' || cripto.trim() === '' ){
+            
+            console.log("cotizando entro en validador");
+            mostrarAlerta();
+            return;
+        }
+
+        //cambiar el state  del API
+        setConsultarAPI(true);
+
+    }
+
+    //Motrar alertas usando el componentes 
+    const mostrarAlerta = ()=>{
+        Alert.alert(
+            'Error..',
+            'Ambos campos  son obligatorios', 
+            [
+                {text:'Ok'}
+            ]
+        )
+    }
+
+
+    useEffect(()=>{
+
+        const consultarAPI = async ()=>{
+
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+            const resultado = await axios.get(url);
+            console.log(resultado.data.Data);
+            setCriptos(resultado.data.Data);
+            //setCriptos( Object.entries(resultado.data.Data) );
+
+        }
+        
+        consultarAPI();
+
+
+
+    }, []); //[]Arreglo de dependencias 
+
+
+
+
     return(
            <View>
                <Text style={styles.label}>Moneda</Text>
+               <Picker
+                    selectedValue={moneda}
+                    onValueChange={ moneda => obtenerMoneda(moneda)  }
+                    itemStyle={{height:120}}
+               >
+                    <Picker.Item label="- Seleccione -"       value="" />                    
+                    <Picker.Item label="- Dolar USA -"        value="USD" />                       
+                    <Picker.Item label="- Peso Mexicano -"    value="MXN" />   
+                    <Picker.Item label="- Euro -"             value="EUR" />                       
+                    <Picker.Item label="- Libra Esterlina -"  value="GBP" />                                           
+                </Picker> 
+
                <Text style={styles.label}>Tipos Crip</Text>
+
+               <Picker
+                    selectedValue={ cripto }
+                    onValueChange={ cripto => obtenerCripto(cripto)  }
+                    itemStyle={{height:120}}
+               >
+                    <Picker.Item label="- Seleccione -"       value="" /> 
+                    { 
+                        criptos.map( listado => (
+                            <Picker.Item key={ listado.CoinInfo.Id } label = { listado.CoinInfo.FullName }  value={listado.CoinInfo.Name} />   
+                        ))
+                        
+                    }                                       
+
+                                         
+                </Picker>
+                <TouchableHighlight
+                    style={styles.btnCotizar}
+                    onPress={ () =>cotizarPrecio() }
+                >
+                     <Text style={styles.txtCotizar}> Calcular </Text>    
+                </TouchableHighlight>                
            </View>        
     );
 };
@@ -15,11 +120,25 @@ const Formulario = () => {
 //Aqui los estilos que vamos a usar 
 const styles = StyleSheet.create({
  label:{
-     //fontFamily:'Arial', 
+     //cd fontFamily:'Arial', 
      textTransform: 'uppercase',
      fontSize:22,
      marginVertical:20, 
        
+ }, 
+ btnCotizar:{
+    backgroundColor:'#5E49E2',
+    padding:10,
+    marginTop:20,
+    
+ }, 
+ txtCotizar:{
+    color:'#FFF',
+    fontSize:18,
+    textTransform:'uppercase',
+    textAlign:'center',
+
+
  }
 
 })                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
